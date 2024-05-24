a4b6d2e154ca185e8415229365b0087a41e1d7ff
sidr467 <146876430+sidr467@users.noreply.github.com>
2024-05-24 16:31:21 +0530

    Later used grid and with image scaling
    used grid for image position for different sizes of scree and for mobile screen image scale by transform

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,22 @@
 module.exports = {
   content: ["*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      screens: {
+        custom: "375px",
+      },
+      colors: {
+        darkcyan: "hsl(158, 36%, 37%)",
+        cream: "hsl(30, 38%, 92%)",
+        verydarkblue: "hsl(212, 21%, 14%)",
+        darkgrayishblue: "hsl(228, 12%, 48%)",
+        white: "hsl(0, 0%, 100%)",
+      },
+      fontFamily: {
+        fraunces: ["Fraunces", "serif"],
+        montserrat: ["Montserrat", "sans-serif"],
+      },
+    },
   },
   plugins: [],
 }
